2081d6bf3ac72104a0d6bb3f8dfdbdf654215459
Hafiz Muhammad Umar Hayat <52619520+MuhammadUmarHayat@users.noreply.github.com>
2024-07-21 18:25:27 +0500

    Add files via upload

---
+++ blog-app-backend/app/Http/Controllers/TempImageController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\TempImage;
+
+
+use Illuminate\Support\Facades\Validator;
+class TempImageController extends Controller
+{
+    
+    public function store(Request $request) {
+
+        // Apply validation
+        $validator = Validator::make($request->all(),[
+            'image' => 'required|image'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Please fix errors',
+                'errors' => $validator->errors()
+            ]);
+        }
+
+        // Upload Image here
+        $image = $request->image;
+        $ext = $image->getClientOriginalExtension();
+        $imageName = time().'.'.$ext;
+
+        // Store Image info in database
+        $tempImage = new TempImage();
+        $tempImage->name = $imageName;
+        $tempImage->save();
+
+        // Move image in temp dir
+        $image->move(public_path('uploads/temp'),$imageName);
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Image uploaded successfully.',
+            'image' => $tempImage
+        ]);
+
+    }
+}
